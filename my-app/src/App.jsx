5769105f183a8fc268fe6ad0d16edae6ee1dc229
RataTudela <ben.olavarria@duocuc.cl>
2025-10-16 12:46:54 -0300

    Arreglado el home su header

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,192 +1,203 @@
 import React from "react";
-import "./styles/Home.css";
-import "./styles/detalleproducot.css";
+import "./styles/CssRegistro.css";
 import "./styles/main.css";
+import "./styles/detalleproducto.css";
+import "./styles/Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
-    <div className="is-preload homepage">
-      <header className="container" id="page-wrapper">
-        <div id="header-wrapper">
-          <header id="header" classname="container">
+    <div id="page-wrapper" className="is-preload homepage">
+      {/* Header */}
+      <div id="header-wrapper">
+        <header id="header" className="container">
+          {/* Logo */}
+          <div id="logo">
+            <h1>
+              <a href="home.html">
+                <img
+                  src="src/images/Logo_de_GameCloud.png"
+                  alt="Logo GameCloud"
+                />
+              </a>
+            </h1>
+          </div>
 
-              {/* Logo */}
-              <div id="logo">
-                <h1>
-                  <a href="home.html">
-                    <img src="src/images/Logo_de_GameCloud.png" alt="Logo GameCloud" style={{ maxWidth: "150px" }}/>
-                  </a>
-                </h1>
-              </div>
-          </header>
           {/* Nav */}
           <nav id="nav">
-								<ul>
-									<li class="current"><a href="home.html">Home</a></li>
-									<li><a href="productos.html">Productos</a></li>
-									<li><a href="inicioSesion.html">Inicio Sesion</a></li>
-									<li><a href="contacto.html">Contacto</a></li>
-									<li><a href="blog.html">Blog</a></li>
-								</ul>
-					</nav>
-        </div>
-      </header>
+            <ul>
+              <li className="current">
+                <a href="home.html">Home</a>
+              </li>
+              <li>
+                <a href="productos.html">Productos</a>
+              </li>
+              <li>
+                <a href="inicioSesion.html">Inicio Sesión</a>
+              </li>
+              <li>
+                <a href="contacto.html">Contacto</a>
+              </li>
+              <li>
+                <a href="blog.html">Blog</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
 
-      {/* Main Content */}
-    <div className="img-fondo">
-      <main className="wrapper">
-        <div className="contenedor-flex">
-          <div className="cajilla-box" style={{ minHeight: "400px" }}>
-            <h1 className="text-center text-dark">
-              <strong>Tienda De Juegos GameCloud</strong>
-            </h1>
-            <h3 className="text-center text-dark mt-3">
-              Somos una tienda de juegos nacida en el año 2025, donde buscamos
-              entregarte los mejores precios en videojuegos, con la mejor
-              calidad, En Chile.
-            </h3>
-            <div className="d-flex justify-content-center mt-4">
-              <a href="productos.html" className="btn btn-dark btn-lg">
+      {/* Main content */}
+      <div className="img-fondo">
+        <div className="wrapper">
+          <div className="contenedor-flex">
+            <div className="cajilla-box">
+              <h1>
+                <strong>Tienda De Juegos GameCloud</strong>
+              </h1>
+              <h3>
+                Somos una tienda de juegos nacida en el año 2025, donde buscamos
+                entregarte los mejores precios en videojuegos, con la mejor
+                calidad, En Chile.
+              </h3>
+              <a className="button" href="productos.html" role="button">
                 Ir a productos
               </a>
             </div>
-          </div>
 
-          {/* Video */}
-          <div className="cajilla2-box" style={{ minHeight: "250px" }}>
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                src="https://www.youtube.com/embed/6XGeJwsUP9c"
-                title="Video Del Dia"
-                allowFullScreen
-              ></iframe>
-            </div>
-            <p className="text-center text-dark mt-3 fs-4">
-              <strong>Video Del Dia</strong>
-            </p>
-          </div>
-        </div>
-
-        {/* Tarjetas de productos */}
-        <div className="row gx-4 justify-content-center">
-          {[
-            {
-              img: "src/images/pic01.jpg",
-              title: "Persona 3: Reload",
-              text: "Persona 3 Reload es un remake del RPG de 2006 Persona 3.",
-            },
-            {
-              img: "src/images/pic02.jpg",
-              title: "God of War: Ragnarok",
-              text: "Secuela de God Of War, donde volvemos a ser Kratos.",
-            },
-            {
-              img: "src/images/pic03.jpg",
-              title: "SilkSong",
-              text: "Secuela de Hollow Knight, Controlamos a Hornet.",
-            },
-            {
-              img: "src/images/pic04.jpg",
-              title: "Lego: Batman",
-              text: "Vuelves Como el Caballero de la noche en forma de Lego.",
-            },
-            {
-              img: "src/images/pic05.jpg",
-              title: "Elden Ring",
-              text: "Nuevo Entrega de FromSoftware, Juego Tipo DarkSouls.",
-            },
-            {
-              img: "src/images/pic06.jpg",
-              title: "Sekiro",
-              text: "Es un Japón ficticio de finales del siglo XVI Reinado por la violencia.",
-            },
-          ].map(({ img, title, text }, i) => (
-            <div className="col-sm-6 col-md-4 col-lg-3 mb-4" key={i}>
-              <div className="card" style={{ width: "100%" }}>
-                <img src={img} className="card-img-top" alt={title} />
-                <div className="card-body">
-                  <h5 className="card-title">{title}</h5>
-                  <p className="card-text">{text}</p>
-                </div>
+            <div className="cajilla2-box">
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                  className="embed-responsive-item"
+                  src="https://www.youtube.com/embed/6XGeJwsUP9c"
+                  title="Video del día"
+                  allowFullScreen
+                ></iframe>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>      
-      {/* Footer */}
-      <footer
-        id="footer-wrapper"
-        className="bg-light border-top border-dark py-4 mt-auto"
-      >
-        <div className="container">
-          <div className="row text-dark">
-            <div className="col-md-4 mb-3">
-              <h3>Confiabilidad</h3>
               <p>
-                Contamos con un sistema de reembolso, con un lapso de 3 días
-                para su devolución, en caso de no recibir nada.
+                <strong>Video Del Día</strong>
               </p>
             </div>
-            <div className="col-md-4 mb-3">
-              <h3>Métodos de pago</h3>
-              <div>
-                <img
-                  src="src/images/mercadopago.png"
-                  alt="MercadoPago"
-                  style={{ maxHeight: "50px", marginRight: "10px" }}
-                />
-                <img
-                  src="src/images/visa.png"
-                  alt="Visa"
-                  style={{ maxHeight: "50px", marginRight: "10px" }}
-                />
-                <img
-                  src="src/images/paypal.webp"
-                  alt="Paypal"
-                  style={{ maxHeight: "50px" }}
-                />
-              </div>
-            </div>
-            <div className="col-md-4 mb-3">
-              <h3>Contáctanos</h3>
-              <ul className="list-inline mb-3">
-                <li className="list-inline-item me-3">
-                  <a href="#" className="text-dark">
-                    <i className="fab fa-twitter"></i> Twitter
-                  </a>
-                </li>
-                <li className="list-inline-item me-3">
-                  <a href="#" className="text-dark">
-                    <i className="fab fa-facebook-f"></i> Facebook
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#" className="text-dark">
-                    <i className="fab fa-instagram"></i> Instagram
-                  </a>
-                </li>
-              </ul>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="emails" className="form-label">
-                    Correo
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="emails"
-                    aria-describedby="emailHelp"
-                  />
-                  <div id="emailError" className="form-text text-danger"></div>
+          </div>
+
+          {/* Cards */}
+          <div className="container">
+            <div className="row">
+              {[
+                {
+                  img: "src/images/pic01.jpg",
+                  title: "Persona 3: Reload",
+                  text: "Persona 3 Reload es un remake del RPG de 2006 Persona 3.",
+                },
+                {
+                  img: "src/images/pic02.jpg",
+                  title: "God of War: Ragnarok",
+                  text: "Secuela de God Of War, donde volvemos a ser Kratos.",
+                },
+                {
+                  img: "src/images/pic03.jpg",
+                  title: "SilkSong",
+                  text: "Secuela de Hollow Knight, Controlamos a Hornet.",
+                },
+                {
+                  img: "src/images/pic04.jpg",
+                  title: "Lego: Batman",
+                  text: "Vuelves Como el Caballero de la noche en forma de Lego.",
+                },
+                {
+                  img: "src/images/pic05.jpg",
+                  title: "Elden Ring",
+                  text: "Nuevo Entrega de FromSoftware, Juego Tipo DarkSouls.",
+                },
+                {
+                  img: "src/images/pic06.jpg",
+                  title: "Sekiro",
+                  text: "Es un Japón ficticio de finales del siglo XVI Reinado por la violencia.",
+                },
+              ].map(({ img, title, text }, i) => (
+                <div className="col-sm" key={i}>
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src={img} className="card-img-top" alt={title} />
+                    <div className="card-body">
+                      <h5 className="card-title">{title}</h5>
+                      <p className="card-text">{text}</p>
+                    </div>
+                  </div>
                 </div>
-              </form>
+              ))}
             </div>
           </div>
         </div>
-      </footer>
+      </div>
+
+      {/* Footer */}
+      <div id="footer-wrapper">
+        <footer id="footer" className="container">
+          <div className="row">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm">
+                  <section className="widget links">
+                    <h3>Confiabilidad</h3>
+                    <p>
+                      Contamos con un sistema de reembolso, con un lapso de 3
+                      días para su devolución, en caso de no recibir nada.
+                    </p>
+                  </section>
+                </div>
+
+                <div className="col-sm">
+                  <section className="widget links">
+                    <h3>Métodos de pago</h3>
+                    <ul className="style2">
+                      <li>
+                        <img
+                          src="src/images/mercadopago.png"
+                          alt="MercadoPago"
+                        />
+                        <img src="src/images/visa.png" alt="Visa" />
+                        <img src="src/images/paypal.webp" alt="Paypal" />
+                      </li>
+                    </ul>
+                  </section>
+                </div>
+
+                <div className="col-sm">
+                  <section className="widget contact last">
+                    <h3>Contáctanos</h3>
+                    <ul>
+                      <li>
+                        <a href="#" className="icon brands fa-twitter">
+                          <span className="label">Twitter</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="icon brands fa-facebook-f">
+                          <span className="label">Facebook</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="icon brands fa-instagram">
+                          <span className="label">Instagram</span>
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="form-group">
+                      <label htmlFor="emails">Correo</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="emails"
+                        aria-describedby="emailHelp"
+                      />
+                      <div id="emailError" className="fore-text"></div>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
-
